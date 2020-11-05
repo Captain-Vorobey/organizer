b5adb500c04a7e9f49850d29f691155d0ce28ec0
Captain-Vorobey <vorobyov_egor@bk.ru>
2020-11-05 15:31:25 +0300

    Added home index to develop

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'home#index'
+  get 'home/index'
 
-  root 'home/index'
-
+  root to: "home#index"
+  
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
