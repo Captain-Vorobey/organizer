3bb6ef94478ca61640e000506ec3567235600f13
Captain-Vorobey <vorobyov_egor@bk.ru>
2020-11-09 16:46:44 +0300

    Resolved conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,7 @@ Rails.application.routes.draw do
 
   root to: "home#index"
   
-<<<<<<< HEAD
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth' }
   get '/auth/:provider/callback', to: 'sessions#create'
-  
-=======
-  devise_for :users
->>>>>>> master
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
