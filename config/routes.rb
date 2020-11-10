955571c926b560d9a5b1a022fc442e56f0bbaa6f
Captain-Vorobey <vorobyov_egor@bk.ru>
2020-11-10 18:30:47 +0300

    Added default root to sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  root to: 'home#index'
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth' }
   get '/auth/:provider/callback', to: 'sessions#create'
