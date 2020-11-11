26d7d3bdd8c869643a51bdc89b93d1a32cdcc7d2
Captain-Vorobey <vorobyov_egor@bk.ru>
2020-11-11 17:37:08 +0300

    Added worked github-auth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+class SessionsController < ApplicationController
+
+  def new; end
+
+  def login; end
+
+  def create; end
+
+  def page_requires_login; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth' }
-  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/auth/:provider/callback', to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
