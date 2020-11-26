d33c09408cc4101a32d1d606793e63ba7f6f857e
Captain-Vorobey <vorobyov_egor@bk.ru>
2020-11-26 11:00:47 +0300

    Corrected misunderstanding

--- db/schema.rb
+++ db/schema.rb
@@ -36,12 +36,14 @@ ActiveRecord::Schema.define(version: 2020_11_18_211016) do
     t.index ["address_id"], name: "index_companies_on_address_id"
   end
 
-  create_table "orders", force: :cascade do |t|
-    t.string "name"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_orders_on_user_id"
+  create_table 'orders', force: :cascade do |t|
+    t.string 'name'
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.bigint 'service_id'
+    t.index ['user_id'], name: 'index_orders_on_user_id'
+    t.index ['service_id'], name: 'index_orders_on_service_id'
   end
 
   create_table "roles", force: :cascade do |t|
