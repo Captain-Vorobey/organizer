21156966a749bfc3fe45ee7427d720065db564f0
Captain-Vorobey <vorobyov_egor@bk.ru>
2020-12-02 19:29:15 +0300

    Used rubocop

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -6,10 +6,12 @@ class OrderController < ApplicationController
   def create
     service = Service.find(params[id])
     allowed_params = order_params.merge(service: service, user: current_user)
-   @order = Order.create(allowed_params)
- end
- private
- def order_params
-   params.require(:order).permit(:name)
- end
+    @order = Order.create(allowed_params)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:name)
+  end
 end
