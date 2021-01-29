377cf09af5afa0751632b5d79dec56e750d1aafd
Captain-Vorobey <vorobyov_egor@bk.ru>
2021-01-29 20:49:24 +0300

    Refactored migrations

--- db/migrate/20210127091836_add_commentable_id_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddCommentableIdToComments < ActiveRecord::Migration[6.0]
-  def change
-    add_column :comments, :commentable_id, :integer
-  end
-end
--- db/migrate/20210127092016_remove_user_id_from_comments.rb
+++ db/migrate/20210127092016_remove_user_id_from_comments.rb
@@ -1,6 +1,5 @@
 class RemoveUserIdFromComments < ActiveRecord::Migration[6.0]
   def change
     remove_column :comments, :user_id, :integer
-    remove_column :comments, :service_id, :integer
   end
 end
